fixed torch compile error when computing stp loss and removed computation of training stp loss

nanogpt/models/gpt.py
@@ -73,8 +73,10 @@ class GPT(nn.Module):
         self.apply(_init_weights)
 
     def forward(
-        self, idx: Tensor, targets: Tensor | None = None, return_logits: bool = True
+        self, idx: Tensor, targets: Tensor | None = None, return_logits: bool = True, return_stp_loss: bool = True
     ) -> tuple[Tensor | None, None]:
+        assert return_stp_loss
+
         # forward the GPT model itself
         x = self.encoder(idx)  # token embeddings of shape (b, t, n_embd)
 

nanogpt/models/mtp.py
@@ -48,7 +48,13 @@ class MultiTokenLM(torch.nn.Module):
             "_autoregressive_mar_mask", IntegrateQuery.scopes_to_mask(self.circuit.circuit, mar_scopes)
         )
 
-    def forward(self, xx: Tensor, yy: Tensor, return_log_probs: bool = False) -> dict[str, Tensor]:
+    def forward(
+        self,
+        xx: Tensor,
+        yy: Tensor,
+        return_log_probs: bool = False,
+        return_stp_loss: bool = False
+    ) -> dict[str, Tensor]:
         # Compute the loss, i.e., the multi-token average negated log-likelihood
 
         # xx: (B, S, D)
@@ -79,9 +85,11 @@ class MultiTokenLM(torch.nn.Module):
         if not return_log_probs:
             log_probs = None
 
-        # TODO: do not compute single token loss, if we do not need it
-        # Compute also the single token loss
-        stp_loss = self.compute_next_token_loss(yy)
+        if return_stp_loss:
+            # Compute also the single token loss, if needed
+            stp_loss = self.compute_next_token_loss(yy)
+        else:
+            stp_loss = None
 
         return dict(log_probs=log_probs, loss=mtp_loss, mtp_loss=mtp_loss, stp_loss=stp_loss)
 
@@ -103,6 +111,7 @@ class MultiTokenLM(torch.nn.Module):
         self._cat_layer.log_probs = cat_log_probs
         self._sum_layer.weight = sum_weight
 
+    @torch._dynamo.disable
     def compute_next_token_loss(self, yy: Tensor) -> Tensor:
         # We keep track of next token prediction loss too, in order to discern
         # how good the model would be for just next token prediction
@@ -113,6 +122,7 @@ class MultiTokenLM(torch.nn.Module):
         stp_loss = -log_probs.mean()
         return stp_loss
 
+    @torch._dynamo.disable
     def compute_next_token_log_probs(self) -> Tensor:
         ########## TODO: to be refactored #########
         # Next token prediction - equivalent to marginalising out future tokens

nanogpt/train.py
@@ -38,7 +38,7 @@ def validation_step(model, val_loader, val_steps, ctx):
     for _ in range(val_steps):
         x_val, y_val = val_loader.next_batch()
         with ctx:
-            results = model(x_val, y_val)
+            results = model(x_val, y_val, return_stp_loss=True)
             val_loss += results['loss'].detach()
             val_stp_loss += results['stp_loss'].detach()
             if 'mtp_loss' in results:
@@ -68,7 +68,6 @@ def training_step(model, train_loader, train_accumulation_steps, optimizer, sche
             results = model(x, y)
             loss = results['loss']
             train_loss = loss.detach()
-            train_stp_loss = results['stp_loss'].detach()
             if 'mtp_loss' in results:
                 train_mtp_loss = results['mtp_loss'].detach()
             else:
@@ -81,7 +80,8 @@ def training_step(model, train_loader, train_accumulation_steps, optimizer, sche
             loss.backward()
 
     for p in model.parameters():
-        p.grad /= train_accumulation_steps
+        if p.requires_grad:
+            p.grad /= train_accumulation_steps
 
     # Add gradient clipping
     torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
@@ -91,7 +91,7 @@ def training_step(model, train_loader, train_accumulation_steps, optimizer, sche
 
     model.zero_grad(set_to_none=True)
 
-    return train_loss, train_stp_loss, train_mtp_loss
+    return train_loss, train_mtp_loss
 
 
 def name_exp(cfg):
@@ -178,7 +178,7 @@ def main(cfg: DictConfig):
                 torch.cuda.synchronize()
 
             # Training step
-            train_loss, train_stp_loss, train_mtp_loss = training_step(
+            train_loss, train_mtp_loss = training_step(
                 model, train_loader, train_accumulation_steps, optimizer, scheduler, ctx
             )
 
@@ -204,7 +204,6 @@ def main(cfg: DictConfig):
             current_lr = optimizer.param_groups[0]['lr']
             logger(f"step:{step}/{cfg.training.num_iterations} train_loss:{train_loss.item():.4f} lr:{current_lr:.6f} time/step:{dt:.2f}s")
             wandb.log({'train/loss': train_loss,
-                       'train/stp_loss': train_stp_loss,
                        'train/mtp_loss': train_mtp_loss,
                        'global_step': step})
     finally:
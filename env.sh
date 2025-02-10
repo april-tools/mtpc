3b69210f115af2214a7b4dba5f4f660fbb47a328
Andreas Grivas <andreasgrv@gmail.com>
2025-02-10 09:32:31 +0000

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 export GPUS=1
 export MTP_ROOT=/set/me/to/root/dirfolder
 export WANDB_MODE=disabled
+export OMP_NUM_THREADS=1
 # Below allows our results to be reproducible
 export CUBLAS_WORKSPACE_CONFIG=:4096:8

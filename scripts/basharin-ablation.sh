344c579eaa876d5aa69aeddfe4339602823593c6
Andreas Grivas <andreasgrv@gmail.com>
2025-03-08 23:26:09 +0000

    Add more exps and add scripts README

--- scripts/basharin-ablation.sh
+++ scripts/basharin-ablation.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-#  n=2, r=1
-torchrun --standalone \
-                --nproc_per_node=$GPUS \
-                -m mtp.train \
-                data=finewebedu10B \
-                training=finewebedu \
-                lm=finewebedu \
-                model=basharin \
-                model.n_token=2 \
-                model.n_component=1 \
-                model.model.gamma=.8 \
-                model.model.beta=.9 \
-                model.sum_weight_head.encoder.n_layer=0 \
-                model.token_head.encoder.n_layer=0 \
-                model.token_head.expander.expander_type=linear \
-                lm.model.freeze=true \
-                lm.model.encoder_only=false \
-                model.mt_head.freeze_unembedding=true \
-                training.device_batch_size=16 \
-                training.expname=basharin-n-2-r-1
-
-##  n=2, r=2
+##  n=2, r=2, beta=0
 torchrun --standalone \
                 --nproc_per_node=$GPUS \
                 -m mtp.train \
@@ -32,7 +11,7 @@ torchrun --standalone \
                 model.n_token=2 \
                 model.n_component=2 \
                 model.model.gamma=.8 \
-                model.model.beta=.9 \
+                model.model.beta=0 \
                 model.sum_weight_head.encoder.n_layer=0 \
                 model.token_head.encoder.n_layer=0 \
                 model.token_head.expander.expander_type=linear \
@@ -40,4 +19,25 @@ torchrun --standalone \
                 lm.model.encoder_only=false \
                 model.mt_head.freeze_unembedding=true \
                 training.device_batch_size=16 \
-                training.expname=basharin-n-2-r-2
+                training.expname=basharin-n-2-r-2-b-0
+
+##  n=2, r=2, beta=0
+torchrun --standalone \
+                --nproc_per_node=$GPUS \
+                -m mtp.train \
+                data=finewebedu10B \
+                training=finewebedu \
+                lm=finewebedu \
+                model=basharin \
+                model.n_token=2 \
+                model.n_component=2 \
+                model.model.gamma=1 \
+                model.model.beta=0 \
+                model.sum_weight_head.encoder.n_layer=0 \
+                model.token_head.encoder.n_layer=0 \
+                model.token_head.expander.expander_type=linear \
+                lm.model.freeze=true \
+                lm.model.encoder_only=false \
+                model.mt_head.freeze_unembedding=true \
+                training.device_batch_size=16 \
+                training.expname=basharin-n-2-r-2-b-0-g-1

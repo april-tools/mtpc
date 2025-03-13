9b65b768e8cd80e7c0a8585142fe3627d617eb68
Andreas Grivas <andreasgrv@gmail.com>
2025-03-13 09:13:39 +0000

    Restrict encoder assertion to training time

--- mtp/models/mtp.py
+++ mtp/models/mtp.py
@@ -64,7 +64,9 @@ class MultiTokenLM(torch.nn.Module):
             assert self.lm.freeze is True, 'Unfreezing LM with KL loss is not currently supported'
             assert self.lm.encoder_only is False, 'We need the LM head to compute KL'
         else:
-            assert self.lm.encoder_only is True, 'We do not need the LM head since we are not computing KL'
+            # We need encoder_only is false during generation - due to speculative decoding
+            if self.training:
+                assert self.lm.encoder_only is True, 'We do not need the LM head since we are not computing KL'
 
 
         # Retrieve the circuit layers to parameterize

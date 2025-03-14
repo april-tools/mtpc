824187a021cfc8ffe48f168ccddae76fbdda6a26
Andreas Grivas <andreasgrv@gmail.com>
2025-03-14 17:46:31 +0000

    Fix check

--- mtp/train.py
+++ mtp/train.py
@@ -222,7 +222,7 @@ def main(cfg: DictConfig):
         logger(f"Validation DataLoader: total number of tokens: {val_loader.ntok_total} across {len(val_loader.files)} files")
         logger(f"During training we will see {ntok_train} tokens")
         logger(f"Each validation step will see {cfg.training.val_tokens} tokens")
-        assert(ntok_train > train_loader.ntok_total, 'Current setup would run multiple epochs on this dataset')
+        assert ntok_train < train_loader.ntok_total, 'Current setup would run multiple epochs on this dataset'
 
         # Calculate steps
         val_steps = cfg.training.val_tokens // (B * T * world_size)

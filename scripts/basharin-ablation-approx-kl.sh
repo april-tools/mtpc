dc1346203a5c2d42f63acac6fd017344d8365340
Andreas Grivas <andreasgrv@gmail.com>
2025-03-20 11:51:47 +0000

    Add test script to check batch size

--- scripts/basharin-ablation-approx-kl.sh
+++ scripts/basharin-ablation-approx-kl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##  n=2, r=2, token_transformer_layer=1
+##  n=4, r=4
 torchrun --standalone \
     --nproc_per_node=$GPUS \
     -m mtp.train \
@@ -9,14 +9,15 @@ torchrun --standalone \
     lm=finewebedu \
     model=basharin \
     model.n_token=4 \
-    model.n_component=1 \
+    model.n_component=4 \
     model.model.gamma=1 \
-    model.model.beta=0 \
-    model.mt_head_hparams.sum_transformer_n_layer=1 \
-    model.mt_head_hparams.tok_transformer_n_layer=1 \
+    model.model.kl_algorithm=binary_approx \
+    model.model.beta=.9 \
+    lm.model.encoder_only=false \
+    model.mt_head_hparams.sum_transformer_n_layer=0 \
+    model.mt_head_hparams.tok_transformer_n_layer=0 \
     model.mt_head_hparams.expander_type=linear \
     lm.model.freeze=true \
-    lm.model.encoder_only=true \
     model.mt_head_hparams.freeze_vocab_unembedding=true \
     training.device_batch_size=16 \
-    training.expname=basharin-apkl-n-4-r-1
+    training.expname=basharin-akl+ce-n-4-r-4

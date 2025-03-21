d2875ef26acacb966009ab1eb9053cd86514eb07
Emile van Krieken <1289165+HEmile@users.noreply.github.com>
2025-03-21 11:51:19 +0000

    Better debugging for creating PVCs

--- eidf/create_pvc.py
+++ eidf/create_pvc.py
@@ -19,17 +19,18 @@ spec:
   storageClassName: csi-rbd-sc
 """
 	print(pvc_script)
-	with open(f"{username}-mtp-{script_name}.yaml", "w") as f:
+	with open(f"{pvc_name(username, script_name)}.yaml", "w") as f:
 		f.write(pvc_script)
 	# Apply the PVC using kubectl
 	import subprocess
 	
-	print(f"Creating PVC {username}-mtp-{script_name}")
-	result = subprocess.run(["kubectl", "apply", "-f", f"{username}-mtp-{script_name}.yaml"], 
+	print(f"Creating PVC {pvc_name(username, script_name)}")
+	result = subprocess.run(["kubectl", "apply", "-f", f"{pvc_name(username, script_name)}.yaml"], 
 		capture_output=True, text=True)
 	
 	if result.returncode == 0:
-		print(f"Successfully created PVC: {username}-mtp-{script_name}")
+		print(result.stdout)
+		print(f"Successfully created PVC: {pvc_name(username, script_name)}")
 	else:
 		print(f"Failed to create PVC: {result.stderr}")
 	

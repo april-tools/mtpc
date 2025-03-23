bccc3c1ff7b0fc0b898bb3bd6674ebe762953e1e
loreloc <lorenzoloconte@outlook.it>
2025-03-23 11:05:40 +0000

    updated script shakespeare charlev

--- scripts/train-shakespeare-charlev.sh
+++ scripts/train-shakespeare-charlev.sh
@@ -5,15 +5,13 @@ n_head=4
 n_embd=256
 
 # Train the autoregressive LLM model
-torchrun --standalone --nproc_per_node=$GPUS -m mtp.train data=shakespeare_char training=shakespeare_char \
-  model=stp \
-  lm.n_layer=$n_layer lm.n_head=$n_head lm.n_embd=$n_embd \
-  lm.model.encoder_only=false \
-  training.save_model_every=100
+#torchrun --standalone --nproc_per_node=$GPUS -m mtp.train data=shakespeare_char training=shakespeare_char \
+#  model=stp \
+#  lm.n_layer=$n_layer lm.n_head=$n_head lm.n_embd=$n_embd \
+#  lm.model.encoder_only=false \
+#  training.save_model_every=100
 
-exit
-
-checkpoint=  # to set
+checkpoint="logs/2025-03-22/18-33-59/model@600.pt"  # to set
 
 # Run the multi-token prediction models
 for model in mtp-cp mtp-hmm;
@@ -33,7 +31,7 @@ do
           lm.n_layer=$n_layer lm.n_head=$n_head lm.n_embd=$n_embd \
           lm.model.encoder_only=false \
           lm.model.freeze=true \
-          lm.model.lm=null lm.model.from_checkpoint=$checkpoint \
+          lm.model.lm=null lm.model.from_checkpoint="$checkpoint" \
           training.save_model_every=100
       done
     done

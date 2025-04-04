1992affe8eaa28ef94d36832e2c39f3a68150d4a
loreloc <lorenzoloconte@outlook.it>
2025-04-04 09:14:25 +0000

    make experiments on much longer token sequences

--- scripts/train-shakespeare-charlev.sh
+++ scripts/train-shakespeare-charlev.sh
@@ -19,7 +19,7 @@ checkpoint="logs/2025-03-22/18-33-59/model@600.pt"  # to set
 # Run the multi-token prediction models
 for model in mtp-cp mtp-hmm;
 do
-  for n_token in 4 6;
+  for n_token in 8 10 12;
   do
     for n_component in 2 4;
     do
@@ -49,7 +49,7 @@ done
 # Run the multi-token prediction models
 for model in mtp-cp mtp-hmm;
 do
-  for n_token in 4 6;
+  for n_token in 8 10 12;
   do
     for n_component in 2 4;
     do

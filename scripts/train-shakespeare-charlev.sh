4c77f62c7def1aa0f5b4c6e0726c62977e7e3abf
loreloc <lorenzoloconte@outlook.it>
2025-04-05 09:22:11 +0000

    resume after disk full crash

--- scripts/train-shakespeare-charlev.sh
+++ scripts/train-shakespeare-charlev.sh
@@ -11,45 +11,45 @@ n_embd=256
 #  model=stp \
 #  lm.n_layer=$n_layer lm.n_head=$n_head lm.n_embd=$n_embd \
 #  lm.model.encoder_only=false \
-#  training.save_model_every=100
+#  training.save_model_every=200
 #  training.expname=stp-shcharlev
 
 checkpoint="logs/2025-03-22/18-33-59/model@600.pt"  # to set
 
 # Run the multi-token prediction models
-for model in mtp-cp mtp-hmm;
-do
-  for n_token in 8 10 12;
-  do
-    for n_component in 2 4;
-    do
-      for freeze in False True;
-      do
-        torchrun --standalone --nproc_per_node=$GPU -m mtp.train data=shakespeare_char training=shakespeare_char \
-          model=$model \
-          model.n_token=$n_token \
-          model.n_component=$n_component \
-          model.model.kl_algorithm=full \
-          model.mt_head_hparams.tok_transformer_n_layer=0 \
-          model.mt_head_hparams.sum_transformer_n_layer=0 \
-          model.mt_head_hparams.expander_type=linear \
-          model.mt_head_hparams.freeze_vocab_unembedding=False \
-          lm.n_layer=$n_layer lm.n_head=$n_head lm.n_embd=$n_embd \
-          lm.model.encoder_only=True \
-          lm.model.freeze=$freeze \
-          lm.model.lm=null lm.model.from_checkpoint="$checkpoint" \
-          training.save_model_every=100 \
-          training.expname=shcharlev-fr-$freeze-linear-$model-n-$n_token-r-$n_component-ce-dsc
-      done
-    done
-  done
-done
+#for model in mtp-cp mtp-hmm;
+#do
+#  for n_token in 8 10 12;
+#  do
+#    for n_component in 2 4;
+#    do
+#      for freeze in False True;
+#      do
+#        torchrun --standalone --nproc_per_node=$GPU -m mtp.train data=shakespeare_char training=shakespeare_char \
+#          model=$model \
+#          model.n_token=$n_token \
+#          model.n_component=$n_component \
+#          model.model.kl_algorithm=full \
+#          model.mt_head_hparams.tok_transformer_n_layer=0 \
+#          model.mt_head_hparams.sum_transformer_n_layer=0 \
+#          model.mt_head_hparams.expander_type=linear \
+#          model.mt_head_hparams.freeze_vocab_unembedding=False \
+#          lm.n_layer=$n_layer lm.n_head=$n_head lm.n_embd=$n_embd \
+#          lm.model.encoder_only=True \
+#          lm.model.freeze=$freeze \
+#          lm.model.lm=null lm.model.from_checkpoint="$checkpoint" \
+#          training.save_model_every=200 \
+#          training.expname=shcharlev-fr-$freeze-linear-$model-n-$n_token-r-$n_component-ce-dsc
+#      done
+#    done
+#  done
+#done
 
 
 # Run the multi-token prediction models
-for model in mtp-cp mtp-hmm;
+for model in mtp-hmm;
 do
-  for n_token in 8 10 12;
+  for n_token in 12;
   do
     for n_component in 2 4;
     do
@@ -68,7 +68,7 @@ do
           lm.model.encoder_only=True \
           lm.model.freeze=$freeze \
           lm.model.lm=null lm.model.from_checkpoint="$checkpoint" \
-          training.save_model_every=100 \
+          training.save_model_every=200 \
           training.expname=shcharlev-fr-$freeze-transf-$model-n-$n_token-r-$n_component-ce-dsc
       done
     done

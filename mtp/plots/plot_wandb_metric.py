a2f91da0abae5f4d92b060465e8f3587ec8a9b62
loreloc <lorenzoloconte@outlook.it>
2025-04-07 12:01:06 +0100

    updated plot_wandb_metric.py

--- mtp/plots/plot_wandb_metric.py
+++ mtp/plots/plot_wandb_metric.py
@@ -6,22 +6,33 @@ import matplotlib.pyplot as plt
 from mtp.plots.utils import setup_tueplots
 
 """
-python mtp/plots/plot_wandb_metric.py --dataset shakespeare_char --filepath cp-hmm-n-4-r-4-valid.pdf --models mtp-cp mtp-hmm --n-component 4 --n-token 4 --metrics kl_loss_at_1 kl_loss_at_2 kl_loss_at_3 kl_loss_at_4 ce_loss_at_1 ce_loss_at_2 ce_loss_at_3 ce_loss_at_4 loss --n-rows 3 --n-cols 4 --share-y-subplot 1 --log-y
-python mtp/plots/plot_wandb_metric.py --dataset shakespeare_char --filepath cp-hmm-n-4-r-4-train.pdf --train-metrics --models mtp-cp mtp-hmm --n-component 4 --n-token 4 --metrics kl_loss_at_1 kl_loss_at_2 kl_loss_at_3 kl_loss_at_4 ce_loss_at_1 ce_loss_at_2 ce_loss_at_3 ce_loss_at_4 loss --n-rows 3 --n-cols 4 --share-y-subplot 1 --log-y
+for n_token in 6 8 10 12;
+do
+  for n_component in 2 4;
+  do
+    python mtp/plots/plot_wandb_metric.py --dataset shakespeare_char --filepath cp-hmm-n-$n_token-r-$n_component-train.pdf \
+      --models mtp-cp mtp-hmm --n-component $n_component --n-token $n_token \
+      --train-metrics --metrics ce_loss_at --n-rows 3 --n-cols 4 --log-y
+  done
+done
 """
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', required=True, help='Dataset the models are trained on.')
     parser.add_argument('--models', nargs='+', required=True, help='List of models by name.')
-    parser.add_argument('--n-component', type=int, required=True, help='The number of components')
+    parser.add_argument('--n-component', type=int, nargs='+', required=True, help='The number of components')
     parser.add_argument('--n-token', type=int, required=True, help='The number of tokens in MTP models')
+    parser.add_argument('--beta', type=float, default=0.0, help="The beta value weighting the KL term in the loss")
+    parser.add_argument('--gamma', type=float, default=0.9, help="The exponential discounting factor hyperparameter for each token loss")
     parser.add_argument('--kl', type=str, choices=['full', 'binary_approx'], default='full', help='The kind of KL objective to use')
-    parser.add_argument('--metrics', nargs='+', required=True, help='Which metrics to plot')
+    parser.add_argument('--metrics', type=str, choices=['kl_loss_at', 'ce_loss_at'], required=True, help='Which metrics to plot')
     parser.add_argument('--train-metrics', action='store_true', default=False, help='Whether to show training metrics instead of validation ones')
+    parser.add_argument('--freeze-lm', action='store_true', default=False, help="Whether to filter the results where the LLM is freezed")
     parser.add_argument('--n-rows', type=int, default=1, help='The number of plot rows')
     parser.add_argument('--n-cols', type=int, default=0, help='The number of plot cols. Defaults to the number of metrics provided.')
     parser.add_argument('--share-y-subplot', type=int, default=-1, help='The subplot column index on which to share the Y-axis of all subplots after it')
+    parser.add_argument('--share-y-all', action='store_true', default=False, help='Whether to share the Y axis of all plots')
     parser.add_argument('--log-y', action='store_true', default=False, help='Whether to plot Ys in log space')
     parser.add_argument('--filepath', type=str, required=True, help='Where to store the figure')
 
@@ -37,11 +48,17 @@ if __name__ == '__main__':
             return False
         if r.config['model']['name'] not in args.models:
             return False
-        if r.config['model']['n_component'] != args.n_component:
+        if r.config['model']['n_component'] not in args.n_component:
             return False
         if r.config['model']['n_token'] != args.n_token:
             return False
-        if r.config['model']['model']['kl_algorithm'] != args.kl:
+        if r.config['lm']['model']['freeze'] != args.freeze_lm:
+            return False
+        if r.config['model']['beta'] != args.beta:
+            return False
+        if r.config['model']['gamma'] != args.gamma:
+            return False
+        if r.config['model']['beta'] != 0.0 and r.config['model']['model']['kl_algorithm'] != args.kl:
             return False
         return True
 
@@ -62,24 +79,49 @@ if __name__ == '__main__':
         hw_ratio=0.75,
         tight_layout=True
     )
-    fig, ax = plt.subplots(n_rows, n_cols, sharex=True, squeeze=False)
+    fig, ax = plt.subplots(n_rows, n_cols, sharex=True, sharey=args.share_y_all, squeeze=False)
 
     metrics_grid = [[None] * n_cols for _ in range(n_rows)]
-    assert len(args.metrics) <= n_rows * n_cols
-    for k, metric in enumerate(args.metrics):
+    metrics = [args.metrics]
+    processed_metrics = []
+    for metric in metrics:
+        if len(metric) <= 3:
+            processed_metrics.append(metric)
+            continue
+        if metric[-3:] != '_at':
+            processed_metrics.append(metric)
+            continue
+        for n in range(1, args.n_token + 1):
+            processed_metrics.append(f'{metric}_{n}')
+
+    assert len(processed_metrics) <= n_rows * n_cols
+    for k, metric in enumerate(processed_metrics):
         i, j = k // n_cols, k % n_cols
-        metrics_grid[i][j] = f"{'train' if args.train_metrics else 'valid'}/{metric}"
+        formatted_metric = f"{'train' if args.train_metrics else 'valid'}/{metric}"
+        metrics_grid[i][j] = formatted_metric
 
     def run_identifier(r) -> str:
         model = r.config['model']['name'].split('-')[1]
-        n_component = r.config['model']['n_component']
-        n_token = r.config['model']['n_token']
-        kl_algorithm = r.config['model']['model']['kl_algorithm']
+        entries = [model]
+        entries.append(f"n={r.config['model']['n_token']}")
+        entries.append(f"r={r.config['model']['n_component']}")
+        beta = r.config['model']['beta']
+        gamma = r.config['model']['gamma']
         transf_tok_n_layer = r.config['model']['mt_head_hparams']['tok_transformer_n_layer']
         transf_sum_n_layer = r.config['model']['mt_head_hparams']['sum_transformer_n_layer']
-        if transf_tok_n_layer == 0 and transf_sum_n_layer == 0:
-            return f'{model} n={n_token} r={n_component} kl={kl_algorithm}'
-        return f'{model} n={n_token} r={n_component} kl={kl_algorithm} tf=t{transf_tok_n_layer}-s{transf_sum_n_layer}'
+        if transf_tok_n_layer != 0 or transf_sum_n_layer != 0:
+            #transf_entries = []
+            #if transf_tok_n_layer != 0:
+            #    transf_entries.append(f't:{transf_tok_n_layer}')
+            #if transf_sum_n_layer != 0:
+            #    transf_entries.append(f's:{transf_sum_n_layer}')
+            #entries.append(f"transf={'-'.join(transf_entries)}")
+            entries.append(f"transf")
+        if beta != 0.0:
+            entries.append(f"kl={r.config['model']['model']['kl_algorithm']}")
+        #if gamma != 1.0:
+        #    entries.append(f"dsc")
+        return ' '.join(entries)
 
     for i in range(n_rows):
         for j in range(n_cols):

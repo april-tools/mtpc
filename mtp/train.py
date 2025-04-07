dbb53d61779a09b08d54c8943260e9e07e6cd2eb
pminervini <p.minervini@gmail.com>
2025-04-07 15:11:48 +0200

    update

--- mtp/train.py
+++ mtp/train.py
@@ -135,10 +135,10 @@ def name_exp(cfg):
 def main(cfg: DictConfig):
     try:
         # Needed for compile
-        # torch._dynamo.config.suppress_errors = True
+        torch._dynamo.config.suppress_errors = True
 
         # NOTE: Below seems needed if freeze=false for some LLMs
-        torch._dynamo.config.optimize_ddp = False
+        # torch._dynamo.config.optimize_ddp = False
 
         set_deterministic(cfg.training.random_seed)
         # Set DEVICE env variable, which is used by mtp.utils.distributed

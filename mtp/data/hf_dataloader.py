3f953621efa8bfd2c21660834b3c4305c6613536
Andreas Grivas <andreasgrv@gmail.com>
2025-04-08 11:54:18 +0100

    Create huggingface dataloader for DDP that follows our current api

--- mtp/data/hf_dataloader.py
+++ mtp/data/hf_dataloader.py
@@ -1,42 +1,91 @@
-import torch
-import numpy as np
-
 from transformers import AutoTokenizer
 from datasets import load_dataset
 from datasets.distributed import split_dataset_by_node
 
 
-class HFDistributedDataLoader:
-    def __init__(self, hf_dataset, model, B, T, process_rank, num_processes, device, split='train'):
+class HFDistributedDataLoader(object):
+    """A generic Huggingfaced Dataloader that is compatible with DDP.
+
+    To override this class, see other examples in the folder.
+    """
+
+    _resolvers = {}
+
+    def __init__(
+        self,
+        hf_dataset: str,
+        hf_model: str,
+        B: int,
+        T: int,
+        process_rank: int,
+        num_processes: int,
+        device: str = "cuda",
+        split="train",
+    ):
+        super().__init__()
         self.hf_dataset = hf_dataset
-        self.process_rank = process_rank
-        self.num_processes = num_processes
+        self.hf_model = hf_model
         self.B = B
         self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
         self.device = device
         self.split = split
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model,
-                model_max_length=self.T,
-                padding_side='right',
-                use_fast=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.hf_model, model_max_length=self.T, padding_side="right", use_fast=True
+        )
 
         self.reset()
 
     def reset(self):
-        self.dataset = load_dataset(self.hf_dataset, split=self.split, streaming=True)
-        self.dataset = split_dataset_by_node(self.dataset, rank=self.process_rank, world_size=self.num_processes)
+        self.dataset = self.load_dataset()
+        self.dataset = split_dataset_by_node(
+            self.dataset, rank=self.process_rank, world_size=self.num_processes
+        )
         self.dataset = self.dataset.batch(self.B)
-        self.dataset = self.dataset.map(lambda x: self.tokenizer(x['text']))
+        self.dataset = self.dataset.map(lambda x: self.process(x))
+
+    def load_dataset(self):
+        return load_dataset(self.hf_dataset, split=self.split, streaming=True)
+
+    def process(self, x):
+        raise NotImplementedError()
 
     def next_batch(self):
-        batch = next(iter(self.dataset.take(1)))
-        x = batch['input_ids'][:, :-1]
-        y = batch['input_ids'][:, 1:]
-        attention_mask = batch['attention_mask'][:, :-1]
-        if self.device == 'cuda':
-            x, y, attention_mask = x.cuda(), y.cuda(), attention_mask.cuda()
-        return dict(input_ids=x, labels=y, attention_mask=attention_mask)
+        fields = next(iter(self.dataset.take(1)))
+        batch = dict()
+        if self.device == "cuda":
+            for k in fields:
+                # Filter outputs to only include needed
+                if k in ("input_ids", "labels", "attention_mask"):
+                    batch[k] = fields[k].cuda()
+        return batch
 
     def seek(self, num_steps):
         self.dataset = self.dataset.skip(num_steps)
+
+    @classmethod
+    def build(
+        cl,
+        hf_dataset: str,
+        hf_model: str,
+        B: int,
+        T: int,
+        process_rank: int,
+        num_processes: int,
+        device: str = "cuda",
+        split="train",
+    ):
+        if hf_dataset in cl._resolvers:
+            return cl._resolvers[hf_dataset](
+                hf_dataset, hf_model, B, T, process_rank, num_processes, device, split
+            )
+        else:
+            return cl(
+                hf_dataset, hf_model, B, T, process_rank, num_processes, device, split
+            )
+
+    @classmethod
+    def register(cl, key, constructor):
+        cl._resolvers[key] = constructor

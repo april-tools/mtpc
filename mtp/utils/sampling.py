9e17884449e6b568968d5dbc3da63752b3f4cde6
Andreas Grivas <andreasgrv@gmail.com>
2025-04-24 13:23:05 +0100

    Implement truncation sampling

---
+++ mtp/utils/sampling.py
@@ -0,0 +1,17 @@
+import torch
+
+
+# Truncation used in Nucleus sampling
+def truncate_probs_top_p(probs, p):
+    # performs the operation on the last dimension
+    argsorts = probs.argsort(dim=-1, descending=True)
+    sorted_probs = torch.gather(probs, -1, argsorts)
+    keep_mask = sorted_probs.cumsum(dim=-1) <= p
+    # Also keep prob of first token that surpasses the threshold
+    keep_mask = torch.scatter(keep_mask, -1, keep_mask.sum(axis=-1, keepdims=True), True)
+    # Map mask back to original ids with inverse perm
+    inv_argsorts = argsorts.argsort(dim=-1)
+    keep_mask = torch.gather(keep_mask, -1, inv_argsorts)
+    truncated_probs = torch.where(keep_mask, probs, torch.zeros_like(probs))
+    truncated_probs = truncated_probs / truncated_probs.sum(dim=-1, keepdims=True)
+    return truncated_probs

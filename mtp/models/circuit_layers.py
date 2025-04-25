63bb439ef897cb7fec186332d425773ff9420238
Andreas Grivas <andreasgrv@gmail.com>
2025-04-25 11:03:57 +0100

    Allow truncating categoricals and mix coeffs via env variable

--- mtp/models/circuit_layers.py
+++ mtp/models/circuit_layers.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import functools
 
@@ -14,6 +15,7 @@ from cirkit.backend.torch.circuits import TorchCircuit
 from cirkit.utils.scope import Scope
 
 from mtp.models.loss import IGNORE_TOKEN_ID
+from mtp.utils.sampling import truncate_probs_top_p
 
 
 def sanitize_input(yy: Tensor) -> Tensor:
@@ -158,6 +160,11 @@ class TorchBatchedCategoricalLayer(TorchExpFamilyLayer):
         # log_probs: (F, B, K, V)
         log_probs = self.log_probs
         probs = torch.exp(log_probs)
+
+        trunc_p = float(os.environ.get('MTP_TRUNC_P', 0.))
+        if trunc_p != 0.:
+            probs = truncate_probs_top_p(probs, p=trunc_p)
+
         dist = distributions.Categorical(probs=probs)
         # samples: (num_samples, F, B, K)
         samples = dist.sample((num_samples,))
@@ -260,13 +267,17 @@ class TorchBatchedSumLayer(TorchInnerLayer):
             raise TypeError(
                 "Sampling in sum layers only works with positive weights summing to 1"
             )
+        probs = weight
+        trunc_p = float(os.environ.get('MTP_TRUNC_P', 0.))
+        if trunc_p != 0.:
+            probs = truncate_probs_top_p(probs, p=trunc_p)
 
         # x: (F, H, Ki, num_samples * B, D) -> (F, H * Ki, num_samples * B, D)
         num_samples = x.shape[3] // weight.shape[1]
         x = x.flatten(1, 2)
 
         # mixing_distribution: (F, B, Ko, H * Ki)
-        mixing_distribution = torch.distributions.Categorical(probs=weight)
+        mixing_distribution = torch.distributions.Categorical(probs=probs)
 
         # mixing_samples: (num_samples, F, B, Ko) -> (F, Ko, num_samples, B) -> (F, Ko, num_samples * B)
         mixing_samples = mixing_distribution.sample((num_samples,))

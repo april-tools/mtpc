ee951d9f2f886af9c6b85fe3b5528b95e1c8ae43
Andreas Grivas <andreasgrv@gmail.com>
2025-05-02 10:58:03 +0100

    Add seq len param to tulu script

--- scripts/data_stats/tulu_example_lengths.py
+++ scripts/data_stats/tulu_example_lengths.py
@@ -9,11 +9,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--num-examples', type=int, default=-1)
+    parser.add_argument('--seq-length', type=int, default=2048 * 4)
 
     args = parser.parse_args()
 
 
-    SEQ_LEN = 2048 * 4
+    # NOTE: We drop examples that do not fit in the sequence length
+    # so changing the sequence length will change the number of outputs
+    SEQ_LEN = args.seq_length
     dl = DistributedDataLoader.resolve(
         "allenai/tulu-3-sft-mixture", "EvaByte/EvaByte", 1, SEQ_LEN, 0, 1, device='cpu'
     )
@@ -26,6 +29,8 @@ if __name__ == "__main__":
             seq_lengths.append(batch['attention_mask'].sum().item())
             toks_to_predict.append((batch['labels'] != IGNORE_TOKEN_ID).sum().item())
             i += 1
+            if i % 10000 == 0:
+                print(f'Processed {i} examples')
             if i == args.num_examples:
                 raise StopIteration()
         except StopIteration:
@@ -34,9 +39,10 @@ if __name__ == "__main__":
     print(f'Iterated over {i} examples')
 
     # Plot histogram
-    plt.hist(seq_lengths, bins=20, edgecolor="black")
-    plt.xlabel("Value")
-    plt.ylabel("Frequency")
-    plt.title("Sequence length of Tulu 3 examples in bytes")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.hist(seq_lengths, bins=20, edgecolor="black")
+    ax.set_xlabel("Value")
+    ax.set_ylabel("Frequency")
+    ax.set_title("Sequence length of Tulu 3 examples in bytes")
     plt.tight_layout()
     plt.show()

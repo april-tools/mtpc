9b39483681bb6e48c792351d23a1c1f0de8a96a9
Andreas Grivas <andreasgrv@gmail.com>
2025-05-04 01:57:55 +0100

    Smaller bs for ff

--- scripts/evabyte-lora-tulu-2k/full-tulu-evabyte-lora-last-8-ff-n-8-r-1.sh
+++ scripts/evabyte-lora-tulu-2k/full-tulu-evabyte-lora-last-8-ff-n-8-r-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # n=8, r=1, lora, fully-factorised
-../uv/uv run torchrun --standalone \
+torchrun --standalone \
     --nproc_per_node=$GPUS \
     -m mtp.train \
     data=tulu3-evabyte \
@@ -12,7 +12,7 @@
     adaptor=lora-last-8 \
     mt_head=linear-evabyte \
     circuit.n_token=8 \
-    training.device_batch_size=8 \
+    training.device_batch_size=4 \
     data.vocab_size=320 \
     model.model.beta=0 \
     model.model.gamma=0.9 \

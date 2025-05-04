781765f9782769c661aefc43c066103b3c572494
Andreas Grivas <andreasgrv@gmail.com>
2025-05-04 01:02:46 +0100

    Add cp and ff scripts

--- scripts/evabyte-lora-tulu-3.5k/full-tulu-evabyte-lora-last-8-cp-n-8-r-8.sh
+++ scripts/evabyte-lora-tulu-3.5k/full-tulu-evabyte-lora-last-8-cp-n-8-r-8.sh
@@ -2,7 +2,7 @@
 
 # Experiments on 3.5k steps on tulu 3 using EvaByte-SFT
 
-# n=8, r=16, lora, cp
+# n=8, r=8, lora, cp
 ../uv/uv run torchrun --standalone \
     --nproc_per_node=$GPUS \
     -m mtp.train \
@@ -14,9 +14,9 @@
     adaptor=lora-last-8 \
     mt_head=linear-evabyte \
     circuit.n_token=8 \
-    circuit.n_component=16 \
-    training.device_batch_size=2 \
+    circuit.n_component=8 \
+    training.device_batch_size=4 \
     data.vocab_size=320 \
     model.model.beta=0 \
     model.model.gamma=0.9 \
-    training.expname=full-tulu-evabyte-lora-last-8-cp-n-8-r-16
+    training.expname=full-tulu-evabyte-lora-last-8-cp-n-8-r-8
--- scripts/evabyte-lora-tulu-3.5k/full-tulu-evabyte-lora-last-8-ff-n-8-r-1.sh
+++ scripts/evabyte-lora-tulu-3.5k/full-tulu-evabyte-lora-last-8-ff-n-8-r-1.sh
@@ -2,7 +2,7 @@
 
 # Experiments on 3.5k steps on tulu 3 using EvaByte-SFT
 
-# n=8, r=16, lora, cp
+# n=8, r=1, lora, fully-factorised
 ../uv/uv run torchrun --standalone \
     --nproc_per_node=$GPUS \
     -m mtp.train \
@@ -10,13 +10,12 @@
     training=tulu3-evabyte-full \
     lm=evabyte \
     model=mtp \
-    circuit=cp \
+    circuit=fully-factorised \
     adaptor=lora-last-8 \
     mt_head=linear-evabyte \
     circuit.n_token=8 \
-    circuit.n_component=16 \
-    training.device_batch_size=2 \
+    training.device_batch_size=8 \
     data.vocab_size=320 \
     model.model.beta=0 \
     model.model.gamma=0.9 \
-    training.expname=full-tulu-evabyte-lora-last-8-cp-n-8-r-16
+    training.expname=full-tulu-evabyte-lora-last-8-ff-n-8-r-1

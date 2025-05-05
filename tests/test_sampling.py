5594624a21500fddc42196890845e99a0b070f7f
Andreas Grivas <andreasgrv@gmail.com>
2025-05-05 12:26:07 +0100

    Add test for equiv of truncation logprobs and probs

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -1,13 +1,13 @@
 import torch
 
-from mtp.utils.sampling import truncate_probs_top_p
+from mtp.utils.sampling import truncate_probs_top_p, truncate_logprobs_top_p
+
 
 def test_categorical_distr_truncation():
     probs = torch.tensor([
         [.05, .5, .2, .25],
         [.1, .4, .4, .1]
     ])
-    
     out = truncate_probs_top_p(probs, p=.5)
 
     expected = torch.tensor([
@@ -16,6 +16,23 @@ def test_categorical_distr_truncation():
     ])
 
     assert torch.allclose(out, expected)
+
+
+def test_truncating_probs_and_logprobs_equiv():
+    probs = torch.tensor([
+        [.05, .5, .2, .25],
+        [.1, .4, .4, .1]
+    ])
+
+    logprobs = torch.log(probs)
+    out = truncate_logprobs_top_p(logprobs, p=.5)
+
+    expected = torch.tensor([
+        [0., 2/3, 0., 1/3],
+        [0., .5, .5, .0]
+    ])
+
+    assert torch.allclose(torch.exp(out), expected)
 
 
 def test_categorical_distr_truncation_one_hot():

a8a34590e311b8253c633c197930f1c281ce5207
Andreas Grivas <andreasgrv@gmail.com>
2025-05-22 15:14:48 +0100

    Fix tests again

--- mtp/data/__init__.py
+++ mtp/data/__init__.py
@@ -73,4 +73,4 @@ class DistributedDataLoader:
 DistributedDataLoader.register(".+\.bin", LocalDistributedDataLoader)
 DistributedDataLoader.register("Aeala/ShareGPT_Vicuna_unfiltered", ShareGPTDataLoader)
 DistributedDataLoader.register("allenai/tulu-3-sft-mixture", TuluDataLoader)
-DistributedDataLoader.register("agrv/tulu-v3-sft-evabyte-seq-len-8196", EvaByteTuluDataLoader)
+DistributedDataLoader.register("agrv/tulu-v3-sft-evabyte-seq-len-8192", EvaByteTuluDataLoader)

28416a638a68eb9958f34f7bdd892d91d7216495
Andreas Grivas <andreasgrv@gmail.com>
2025-09-12 11:57:31 +0100

    Shorten hash

--- mtp/utils/timestamp.py
+++ mtp/utils/timestamp.py
@@ -5,5 +5,5 @@ import datetime
 def unique_timestamp():
     now = datetime.datetime.now()
     formatted = now.strftime("%Y-%m-%d-%H-%M-%S")
-    unique_suffix = str(uuid.uuid4())[:8]
+    unique_suffix = str(uuid.uuid4())[:4]
     return f"{formatted}-{unique_suffix}"

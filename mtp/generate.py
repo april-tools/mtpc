5a571002983579763186cc654876868fef80bf57
Andreas Grivas <andreasgrv@gmail.com>
2025-09-12 23:39:20 +0100

    Add subset index to stats

--- mtp/generate.py
+++ mtp/generate.py
@@ -603,6 +603,7 @@ if __name__ == "__main__":
     stats["exp_host"] = socket.gethostname()
     stats["model"] = cfg.model.model._target_
     stats["random_seed"] = args.random_seed
+    stats["prompt_subset_index"] = args.prompt_subset_index
     stats["ntoken"] = n_token
     stats["ncomponent"] = n_component
     stats["task"] = args.task

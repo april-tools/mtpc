dd05fa5e06608da073b8fe24b752a4bc6c89f985
Andreas Grivas <andreasgrv@gmail.com>
2025-09-18 20:46:32 +0100

    Add script to generate table 1

--- scripts/plots/plots.sh
+++ scripts/plots/plots.sh
@@ -50,3 +50,7 @@ do
 		python mtp/plots/plot_throughput_speculative.py "outputs/results/$GPU/throughput-sampling-$model.jsonl" --step 900 --ntokens 1 8 16 --ncomponent 1 32 --decoding sampling --id throughput-$GPU-$model --save > /dev/null
 	done
 done
+
+
+# Table 1
+python mtp/tables/table_1_cp_rank_comparison.py --raw-throughput-file outputs/results/RTX-3090/throughput_evabyte_full-run-100_100.jsonl --spec-throughput-file outputs/results/RTX-3090/throughput-evabyte-sampling-no-lora-1024-250.jsonl > "outputs/tables/throughput-RTX-3090-no-lora-cp-comparsion.txt"

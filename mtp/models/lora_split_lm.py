6a61dc3a4fae2d1375f79dd364f4aa0b412ac41c
Andreas Grivas <andreasgrv@gmail.com>
2025-09-23 13:43:33 +0100

    Correct lora_split_lm to run with no errors

--- mtp/models/lora_split_lm.py
+++ mtp/models/lora_split_lm.py
@@ -1,5 +1,6 @@
 import torch
 from copy import deepcopy
+from torch import Tensor
 
 from mtp.models.evabyte.eva_cache import EvaStaticCacheForTriton
 from mtp.models.evabyte.multibyte_decoding_evabyte import (
@@ -21,16 +22,22 @@ def count_adapter_layers(lm):
     return count
 
 
-def get_position_ids(inputs, cache):
+def get_position_ids(inputs, cache, target=None):
+    assert target in {"draft", "verifier", None}
     # Construct input_ids
     if cache is None:
         position_ids = torch.arange(
             0, inputs.shape[1], device=inputs.device, dtype=torch.int
         ).unsqueeze(dim=0)
     else:
+        assert target is not None
         past_seen_tokens = cache.get_seq_length()
+        start = past_seen_tokens if target == "draft" else past_seen_tokens - 1
         position_ids = torch.arange(
-            past_seen_tokens, inputs.shape[1], device=inputs.device, dtype=torch.int
+            start,
+            inputs.shape[1],
+            device=inputs.device,
+            dtype=torch.int,
         ).unsqueeze(dim=0)
     return position_ids
 
@@ -44,12 +51,13 @@ class LoRASplitLM(torch.nn.Module):
     """
 
     def __init__(
-        self, shared_encoder, draft_encoder, verifier_encoder, split_layer_idx
+        self, shared_encoder, draft_encoder, verifier_encoder, lm_head, split_layer_idx
     ):
         super().__init__()
         self.shared_encoder = shared_encoder
         self.draft_encoder = draft_encoder
         self.verifier_encoder = verifier_encoder
+        self.lm_head = lm_head
         self.split_layer_idx = split_layer_idx
 
         self.reset_caches()
@@ -63,6 +71,9 @@ class LoRASplitLM(torch.nn.Module):
             lm: The original LM instance
             split_layer_idx: Layer index to split at. If None, uses num_layers - num_adapter_layers
         """
+
+        lm_head = deepcopy(lm.model.lm_head)
+        del lm.model.lm_head
 
         num_adapter_layers = count_adapter_layers(lm.model.model)
         num_total_layers = len(lm.model.model.layers)
@@ -88,37 +99,43 @@ class LoRASplitLM(torch.nn.Module):
         # NOTE: ! Important !
         # Monkey-patch norm since it would be applied to the last activation giving wrong result
         shared_encoder.model.model.norm = torch.nn.Identity()
-        shared_encoder.config.num_hidden_layers = len(shared_encoder.model.model.layers)
+        shared_encoder = shared_encoder.unload()
+        shared_encoder.config.num_hidden_layers = len(shared_encoder.model.layers)
 
         # ===================== Draft LoRA Encoder ============================
         draft_encoder.model.model.layers = deepcopy(all_layers[split_layer_idx:])
+        # Correct the layer idx to match the new cache
+        for idx, layer in enumerate(draft_encoder.model.model.layers):
+            layer.self_attn.layer_idx = idx
         # Merge LoRA weights
         draft_encoder = draft_encoder.merge_and_unload()
-        draft_encoder.config.num_hidden_layers = len(draft_encoder.model.model.layers)
+        draft_encoder.config.num_hidden_layers = len(draft_encoder.model.layers)
 
         # ================== Verifier no LoRA Encoder =========================
         verifier_encoder.model.model.layers = deepcopy(all_layers[split_layer_idx:])
+        # Correct the layer idx to match the new cache
+        for idx, layer in enumerate(verifier_encoder.model.model.layers):
+            layer.self_attn.layer_idx = idx
         verifier_encoder = verifier_encoder.unload()
-        verifier_encoder.config.num_hidden_layers = len(
-            verifier_encoder.model.model.layers
-        )
+        verifier_encoder.config.num_hidden_layers = len(verifier_encoder.model.layers)
 
         del all_layers
 
-        return cls(shared_encoder, draft_encoder, verifier_encoder, split_layer_idx)
+        return cls(shared_encoder, draft_encoder, verifier_encoder, lm_head, split_layer_idx)
 
-    def prefill(self, input_ids):
+    @torch.no_grad()
+    def prefill(self, input_ids, circuit_n_token):
         use_cache = True
 
         # Initialises the cache
-        self.init_caches()
+        self.init_caches(circuit_n_token, batch_size=input_ids.shape[0])
 
         position_ids = get_position_ids(input_ids, None)
 
         # NOTE: No need to update cache as multibyte_decoding=False
         # appends to the cache in the forward pass
         # ============ Prefill: Shared Encoder ========================
-        shared_outputs = self.shared_encoder(
+        shared_outputs = self.shared_encoder.model(
             input_ids=input_ids,
             use_cache=use_cache,
             position_ids=position_ids,
@@ -128,7 +145,7 @@ class LoRASplitLM(torch.nn.Module):
         shared_hidden_state = shared_outputs["last_hidden_state"]
 
         # ============ Prefill: Draft Encoder ========================
-        draft_outputs = self.draft_encoder(
+        draft_outputs = self.draft_encoder.model(
             input_ids=input_ids,
             inputs_embeds=shared_hidden_state,
             use_cache=use_cache,
@@ -138,7 +155,7 @@ class LoRASplitLM(torch.nn.Module):
         )
         draft_hidden_state = draft_outputs["last_hidden_state"]
         # ============ Prefill: Verifier Encoder ========================
-        verifier_outputs = self.verifier_encoder(
+        verifier_outputs = self.verifier_encoder.model(
             input_ids=input_ids,
             inputs_embeds=shared_hidden_state,
             use_cache=use_cache,
@@ -150,22 +167,22 @@ class LoRASplitLM(torch.nn.Module):
 
         # Update kv cache
         shared_outputs["past_key_values"] = (
-            self.shared_encoder.lm_model._multi_byte_pred_update_cache_when_prefil_len_eq_window_size(
+            self.shared_encoder._multi_byte_pred_update_cache_when_prefil_len_eq_window_size(
                 shared_outputs["past_key_values"]
             )
         )
         draft_outputs["past_key_values"] = (
-            self.draft_encoder.lm_model._multi_byte_pred_update_cache_when_prefil_len_eq_window_size(
+            self.draft_encoder._multi_byte_pred_update_cache_when_prefil_len_eq_window_size(
                 draft_outputs["past_key_values"]
             )
         )
         verifier_outputs["past_key_values"] = (
-            self.verifier_encoder.lm_model._multi_byte_pred_update_cache_when_prefil_len_eq_window_size(
+            self.verifier_encoder._multi_byte_pred_update_cache_when_prefil_len_eq_window_size(
                 verifier_outputs["past_key_values"]
             )
         )
 
-        self.update_caches(
+        self.set_caches(
             shared_cache=shared_outputs["past_key_values"],
             draft_cache=draft_outputs["past_key_values"],
             verifier_cache=verifier_outputs["past_key_values"],
@@ -178,13 +195,45 @@ class LoRASplitLM(torch.nn.Module):
         )
         return results
 
-    def draft(self, input_ids, use_cache=True, shared_hidden_state=None):
+    @property
+    def head(self):
+        return getattr(self, "lm_head")
+
+    def head_logits(self, xx: Tensor) -> Tensor:
+        # Compute the logits with the head
+        logits = self.head(xx)
+
+        # Checker whether the LM is multi-token model
+        # In that case, return the logits of the first part of the head only
+        if (
+            hasattr(self._lm.config, "num_pred_heads")
+            and self._lm.config.num_pred_heads > 1
+        ):
+            num_pred_heads, vocab_size = (
+                self._lm.config.num_pred_heads,
+                self._lm.config.vocab_size,
+            )
+            assert logits.shape == (
+                logits.shape[0],
+                logits.shape[1],
+                num_pred_heads * vocab_size,
+            )
+            logits = logits.view(
+                logits.shape[0], logits.shape[1], num_pred_heads, vocab_size
+            )
+            logits = logits[:, :, 0]  # (B, S, V)
+
+        # Cast to float32
+        return logits.float()
+
+    @torch.no_grad()
+    def draft(self, input_ids, shared_hidden_state=None, use_cache=True):
 
         if not use_cache:
             raise ValueError("use_cache=False not fully implemented")
 
         if use_cache:
-            position_ids = get_position_ids(input_ids, self.shared_encoder_cache)
+            position_ids = get_position_ids(input_ids, self.shared_encoder_cache, "draft")
             assert self.shared_encoder_cache is not None, "Prefilling required"
             assert self.draft_encoder_cache is not None, "Prefilling required"
             shared_past_key_values = self.shared_encoder_cache
@@ -196,7 +245,7 @@ class LoRASplitLM(torch.nn.Module):
             attn_mask = multi_byte_pred_prepare_attn_mask(
                 self.shared_encoder.config,
                 past_seen_tokens,
-                input_ids[1] - past_seen_tokens,
+                input_ids.shape[1] - past_seen_tokens,
                 device=input_ids.device,
             )
         else:
@@ -207,8 +256,8 @@ class LoRASplitLM(torch.nn.Module):
 
         if shared_hidden_state is None:
             # Run shared_encoder
-            shared_outputs = self.shared_encoder(
-                input_ids=input_ids,
+            shared_outputs = self.shared_encoder.model(
+                input_ids=input_ids[:, past_seen_tokens:],
                 use_cache=use_cache,
                 attention_mask=attn_mask,
                 position_ids=position_ids,
@@ -219,10 +268,11 @@ class LoRASplitLM(torch.nn.Module):
             shared_past_key_values = shared_outputs["past_key_values"]
 
         # Run draft_encoder
-        draft_outputs = self.draft_encoder(
-            input_ids=input_ids,
+        draft_outputs = self.draft_encoder.model(
+            input_ids=input_ids[:, past_seen_tokens:],
             inputs_embeds=shared_hidden_state,
             use_cache=use_cache,
+            attention_mask=attn_mask,
             past_key_values=draft_past_key_values,
             position_ids=position_ids,
             multibyte_decoding=use_cache,
@@ -237,7 +287,8 @@ class LoRASplitLM(torch.nn.Module):
         )
         return results
 
-    def verify(self, input_ids, use_cache=True, shared_hidden_state=None):
+    @torch.no_grad()
+    def verify(self, input_ids, shared_hidden_state=None, use_cache=True):
 
         if not use_cache:
             raise ValueError("use_cache=False not fully implemented")
@@ -249,17 +300,15 @@ class LoRASplitLM(torch.nn.Module):
             verifier_past_key_values = self.verifier_encoder_cache
             past_seen_tokens = self.shared_encoder_cache.get_seq_length()
             assert (
-                past_seen_tokens == self.veriifier_encoder_cache.get_seq_length()
-            ), "draft and shared cache out of sync"
+                past_seen_tokens == self.verifier_encoder_cache.get_seq_length()
+            ), "verifier and shared cache out of sync"
             attn_mask = multi_byte_pred_prepare_attn_mask(
                 self.shared_encoder.config,
                 past_seen_tokens,
-                input_ids[1] - past_seen_tokens + 1,
+                input_ids.shape[1] - past_seen_tokens + 1,
                 device=input_ids.device,
             )
-            position_ids = get_position_ids(input_ids, self.shared_encoder_cache)
-            # TODO: Below is correct
-            # position_ids = torch.arange(past_seen_tokens - 1, input_ids.shape[1], device=input_ids.device, dtype=torch.int).unsqueeze(dim=0)
+            position_ids = get_position_ids(input_ids, self.shared_encoder_cache, "verifier")
         else:
             position_ids = None
             shared_past_key_values = None
@@ -268,7 +317,7 @@ class LoRASplitLM(torch.nn.Module):
         if shared_hidden_state is None:
             # Run shared_encoder
             # TODO: Below should be input_ids[:, past_seen_tokens - 1 :]
-            shared_outputs = self.shared_encoder(
+            shared_outputs = self.shared_encoder.model(
                 input_ids=input_ids[:, past_seen_tokens - 1 :],
                 use_cache=use_cache,
                 attention_mask=attn_mask,
@@ -280,7 +329,7 @@ class LoRASplitLM(torch.nn.Module):
             shared_past_key_values = shared_outputs["past_key_values"]
 
         # Run verifier_encoder
-        verifier_outputs = self.draft_encoder(
+        verifier_outputs = self.verifier_encoder.model(
             input_ids=input_ids[:, past_seen_tokens - 1 :],
             inputs_embeds=shared_hidden_state,
             use_cache=use_cache,
@@ -299,11 +348,11 @@ class LoRASplitLM(torch.nn.Module):
         )
         return results
 
-    def init_caches(self, batch_size=1):
+    def init_caches(self, num_tokens_speculate, batch_size=1):
         self.shared_encoder_cache = EvaStaticCacheForTriton(
             batch_size,
             self.shared_encoder.config.num_attention_heads,
-            self.shared_encoder.config.window_size + self.circuit.n_token,
+            self.shared_encoder.config.window_size + num_tokens_speculate,
             self.shared_encoder.config.hidden_size
             // self.shared_encoder.config.num_attention_heads,
             self.shared_encoder.config.num_hidden_layers,
@@ -313,7 +362,7 @@ class LoRASplitLM(torch.nn.Module):
         self.draft_encoder_cache = EvaStaticCacheForTriton(
             batch_size,
             self.draft_encoder.config.num_attention_heads,
-            self.draft_encoder.config.window_size + self.circuit.n_token,
+            self.draft_encoder.config.window_size + num_tokens_speculate,
             self.draft_encoder.config.hidden_size
             // self.draft_encoder.config.num_attention_heads,
             self.draft_encoder.config.num_hidden_layers,
@@ -323,7 +372,7 @@ class LoRASplitLM(torch.nn.Module):
         self.verifier_encoder_cache = EvaStaticCacheForTriton(
             batch_size,
             self.verifier_encoder.config.num_attention_heads,
-            self.verifier_encoder.config.window_size + self.circuit.n_token,
+            self.verifier_encoder.config.window_size + num_tokens_speculate,
             self.verifier_encoder.config.hidden_size
             // self.verifier_encoder.config.num_attention_heads,
             self.verifier_encoder.config.num_hidden_layers,
@@ -331,7 +380,7 @@ class LoRASplitLM(torch.nn.Module):
             self.verifier_encoder.device,
         )
 
-    def update_caches(self, shared_cache=None, draft_cache=None, verifier_cache=None):
+    def set_caches(self, shared_cache=None, draft_cache=None, verifier_cache=None):
         if shared_cache is not None:
             self.shared_encoder_cache = shared_cache
         if draft_cache is not None:

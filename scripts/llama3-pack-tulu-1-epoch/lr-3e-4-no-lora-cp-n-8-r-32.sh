ae6fe37f90d08f6c110917d759040c09970060d4
Andreas Grivas <andreasgrv@gmail.com>
2025-11-27 00:34:27 +0000

    Add experiment scripts

---
+++ scripts/llama3-pack-tulu-1-epoch/lr-3e-4-no-lora-cp-n-8-r-32.sh
@@ -0,0 +1,20 @@
+# n=8, r=32, no lora, cp
+
+torchrun --standalone \
+    --nproc_per_node=$GPUS \
+	-m mtp.train \
+	data=tulu3-llama3-packed \
+	training=tulu3-evabyte-1epoch \
+	lm=llama3-2-3b-byte \
+	model=mtp \
+	adaptor=none \
+	mt_head=linear-evabyte \
+	circuit=cp \
+	circuit.n_token=8 \
+	circuit.n_component=32 \
+	training.device_batch_size=1 \
+	model.model.beta=0 \
+	model.model.gamma=0.9 \
+    data.val_bin=null \
+    training.learning_rate=0.0003 \
+    training.expname=llama-lr-3e-4-no-lora-cp-n-8-r-32

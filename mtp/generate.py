1fe2bf9ffe1d4959275eea84ca8eb99ccf4c0524
Andreas Grivas <andreasgrv@gmail.com>
2025-12-02 00:45:53 +0000

    Log mean acc rate as null if not speculative

--- mtp/generate.py
+++ mtp/generate.py
@@ -711,13 +711,13 @@ if __name__ == "__main__":
         for i in range(len(prompts)):
             log = dict()
             log["total_generated_tokens"] = sum(all_num_generated_tokens[i])
-            log["avg_accepted_tokens"] = np.mean(all_num_accepted_tokens[i])
             log["elapsed_time"] = [round(t, 6) for t in all_elapsed_times[i]]
             log["prefill_time"] = round(prefill_times[i], 6)
             log["prompt"] = prompts[i]
             log["num_generated_tokens"] = all_num_generated_tokens[i]
-            log["num_accepted_tokens"] = all_num_accepted_tokens[i]
             log["generated_tokens"] = all_generated_tokens[i]
+            log["avg_accepted_tokens"] = np.mean(all_num_accepted_tokens[i]) if args.speculative else None
+            log["num_accepted_tokens"] = all_num_accepted_tokens[i] if args.speculative else None
             log_entries.append("%s\n" % json.dumps(log))
         with open(file_path, "w") as f:
             f.writelines(log_entries)

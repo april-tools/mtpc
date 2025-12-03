a308ec813dc9d6defb88ab05cf770489326fd297
Andreas Grivas <andreasgrv@gmail.com>
2025-12-03 00:13:35 +0000

    Add scripts for Llama runs

---
+++ scripts/throughput/llama/throughput-argmax-no-lora-1-900.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$MTP_ROOT/bin/compute_throughput_speculative_argmax outputs/models/llama/no-lora llama-no-lora 1 900
---
+++ scripts/throughput/llama/throughput-sampling-no-lora-0-900.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$MTP_ROOT/bin/compute_throughput_speculative outputs/models/llama/no-lora llama-no-lora 0 900

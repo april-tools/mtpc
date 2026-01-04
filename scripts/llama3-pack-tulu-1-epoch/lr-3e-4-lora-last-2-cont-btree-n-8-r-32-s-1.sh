46ed548abba0ddb7e57931d31af1090a7b76f1cf
Andreas Grivas <andreasgrv@gmail.com>
2026-01-04 13:04:19 +0000

    Create n=8 scripts

--- scripts/llama3-pack-tulu-1-epoch/lr-3e-4-lora-last-2-cont-btree-n-8-r-32-s-1.sh
+++ scripts/llama3-pack-tulu-1-epoch/lr-3e-4-lora-last-2-cont-btree-n-8-r-32-s-1.sh
@@ -3,19 +3,20 @@
 torchrun --standalone \
     --nproc_per_node=$GPUS \
     -m mtp.train \
-    load_mtp_head_from_model=outputs/models/no-lora/llama-lr-3e-4-no-lora-ff-n-16-r-1/model@900.pt \
+    load_mtp_head_from_model=outputs/models/llama/no-lora/llama-lr-3e-4-no-lora-btree-n-8-r-32-s-1/model@900.pt \
 	data=tulu3-llama3-packed \
     training=tulu3-evabyte-1epoch \
 	lm=llama3-2-3b-byte \
     model=mtp \
     adaptor=lora-last-2 \
     mt_head=linear-evabyte \
-    circuit=fully_factorized \
-    circuit.n_token=16 \
-    circuit.n_component=1 \
+    circuit=btree \
+    circuit.n_token=8 \
+    circuit.n_component=32 \
+    circuit.n_repetition=1 \
     training.device_batch_size=1 \
     model.model.beta=0 \
     model.model.gamma=0.9 \
     data.val_bin=null \
     training.learning_rate=0.0003 \
-    training.expname=llama-lr-3e-4-lora-last-2-cont-ff-n-16-r-1
+    training.expname=llama-lr-3e-4-lora-last-2-cont-btree-n-8-r-32-s-1
